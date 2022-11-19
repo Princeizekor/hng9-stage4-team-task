f98c479e817a9d494f49124c12657abd168981e1
IZEKOR PAUL <izekorpaul297@gmail.com>
2022-11-19 10:51:36 +0100

    feat: added new feature

--- src/Navbar.js
+++ src/Navbar.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom'
 
 function Navbar() {
     const [sidebar, setSidebar] = useState(false)
+    const [close, setClose] = useState(true)
     return (
         <div className="navbar">
             <img src="../img/Frame.svg" alt="page logo" className="logo"/>
@@ -27,33 +28,12 @@ function Navbar() {
                 <a href="" className="started">Get started</a>
             </div>
             </ul>
-            <ul className="lists" style={sidebar !== false ? {height: '20vh', top: '70px'} : {top: '-200px', overflow: 'hidden'}}>
-                <li>
-                    <Link to="">About us</Link>
-                </li>
-                <li>
-                    <Link to="">Pricing plan</Link>
-                </li>
-                <li>
-                    <Link to="">Blog</Link>
-                </li>
-                <li>
-                    <Link to="">FAQs</Link>
-                </li>
-                <li>
-                    <Link to="">Contact us</Link>
-                </li>
-                <div className="btns">
-                <a href="" className="signin">Sign in</a>
-                <a href="" className="started">Get started</a>
-            </div>
-            </ul>
             <div className="buttons">
                 <a href="" className="sign-in">Sign in</a>
                 <a href="" className="start">Get started</a>
             </div>
+            <img onClick={() => setClose(close)} src="./img/Vector (1).svg" alt="close menu image" className="close"/>
             <img onClick={() => setSidebar(!sidebar)} src="./img/sandwich (1).svg" alt="hamburger opener image" className="menu"/>
-            <img onClick={() => setSidebar(!sidebar)} src="./img/sandwich (1).svg" alt="hamburger opener image" className="menus"/>
         </div>
     )
 }
